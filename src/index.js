6313c8b5316e3c2061767ee318fa1fb0115699a6
condef5 <davis.con.fab@gmail.com>
2019-07-29 22:55:45 -0500

    Update styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const $style = document.createElement('style');
-const $area = document.createElement('div');
+const $style = document.createElement("style");
+const $area = document.createElement("div");
 
-$area.classList.add('area');
+$area.classList.add("area");
 document.body.appendChild($area);
 
 $style.innerHTML = `
@@ -11,28 +11,34 @@ $style.innerHTML = `
   right: 20px;
   z-index: 2000;
   transition: transform 0.4s ease 0s;
+  cursor: default;
 }
 
 .notify {
   display: flex;
-  justify-content: space-between;
   align-items: center;
   margin-bottom: 10px;
   border-radius: 5px;
-  box-shadow: rgba(0, 0, 0, 0.12) 0px 4px 9px;
-  font-size: 14px;
-  height: 60px;
-  width: 300px;
-  padding: 0px 20px;
+  font-size: 0.875rem;
+  box-shadow: 0px 3px 5px -1px rgba(0,0,0,0.2),
+    0px 6px 10px 0px rgba(0,0,0,0.14), 
+    0px 1px 18px 0px rgba(0,0,0,0.12);
+  padding: 14px 16px;
+  min-width: 70px;
+  max-width: 350px;
+  letter-spacing: 0.01071em;
+  justify-content: center;
+  line-height: 1.43;
 }
 
 .light {
   background: white;
   color: black;
+  box-shadow: rgba(0, 0, 0, 0.12) 0px 4px 9px;
 }
 
 .dark {
-  background-color: black;
+  background-color: rgba(0, 0, 0, 0.85);
   color: #fff;
 }
 
@@ -69,18 +75,25 @@ $style.innerHTML = `
 `;
 document.body.appendChild($style);
 
-function notifySpace({ text, duration = 2000, theme = 'light' }) {
-  const $notify = document.createElement('div');
-  $notify.classList.add('notify', theme, 'up');
+function notifySpace(props) {
+  const defaults = { duration: 3000, theme: "dark" };
+  const settings =
+    typeof props === "string"
+      ? { ...defaults, text: props }
+      : { ...defaults, ...props };
+
+  const { text, duration, theme } = settings;
+  const $notify = document.createElement("div");
+  $notify.classList.add("notify", theme, "up");
   $notify.innerText = text;
 
   $area.appendChild($notify);
   setTimeout(() => {
-    $notify.classList.add('down');
+    $notify.classList.add("down");
     setTimeout(() => {
       $area.removeChild($notify);
-    }, 500);
+    }, 1000);
   }, duration);
 }
 
-export default notifySpace;
+module.exports = notifySpace;
